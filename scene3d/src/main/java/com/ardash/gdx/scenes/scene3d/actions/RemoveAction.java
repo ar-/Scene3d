ab695f71202a0eb04d7900329be8e6ce0950ac65
Z <z@z>
2020-04-11 10:36:47 +0200

    moved package

---
+++ scene3d/src/main/java/com/ardash/gdx/scenes/scene3d/actions/RemoveAction.java
@@ -0,0 +1,40 @@
+package com.ardash.gdx.scenes.scene3d.actions;
+
+import com.ardash.gdx.scenes.scene3d.Action3D;
+import com.ardash.gdx.scenes.scene3d.Actor3D;
+
+/** Removes an action from an actor.
+ * @author Nathan Sweet */
+public class RemoveAction extends Action3D {
+        private Actor3D target;
+        private Action3D action;
+
+        @Override
+        public boolean act (float delta) {
+                (target != null ? target : actor).removeAction(action);
+                return true;
+        }
+
+        public Actor3D getTarget() {
+                return target;
+        }
+
+        public void setTarget(Actor3D actor) {
+                this.target = actor;
+        }
+
+        public Action3D getAction () {
+                return action;
+        }
+
+        public void setAction(Action3D action) {
+                this.action = action;
+        }
+
+        @Override
+        public void reset () {
+                super.reset();
+                target = null;
+                action = null;
+        }
+}
---
+++ scene3d/src/main/java/com/ardash/gdx/scenes/scene3d/shape/CylinderActor3D.java
@@ -0,0 +1,52 @@
+package com.ardash.gdx.scenes.scene3d.shape;
+
+import com.ardash.gdx.scenes.scene3d.Actor3D;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+
+/**
+ * Created by boris on 24/05/2017.
+ */
+
+public class CylinderActor3D extends Actor3D {
+
+    public CylinderActor3D(float radius, float height) {
+        this(radius, height, Color.WHITE);
+    }
+
+    public CylinderActor3D(float radius, float height, Color color) {
+        this(radius, height, color, new ModelBuilder());
+    }
+
+    public CylinderActor3D(float radius, float height, ModelBuilder modelBuilder) {
+        this(radius, height, Color.WHITE, modelBuilder);
+    }
+
+    public CylinderActor3D(float radius, float height, Color color, ModelBuilder modelBuilder) {
+        super(createModel(radius, height, color, null, modelBuilder));
+    }
+
+    public CylinderActor3D(float radius, float height, Texture texture, ModelBuilder modelBuilder) {
+        super(createModel(radius, height, null, texture, modelBuilder));
+    }
+
+    public CylinderActor3D(float radius, float height, Color color, Texture texture, ModelBuilder modelBuilder) {
+        super(createModel(radius, height, color, texture, modelBuilder));
+    }
+
+    private static Model createModel(float radius, float height, Color color, Texture texture, ModelBuilder modelBuilder) {
+        Material material = new Material();
+        if (color != null) material.set( ColorAttribute.createDiffuse(color) );
+        if (texture != null) material.set( TextureAttribute.createDiffuse(texture) );
+
+        int usageCode = VertexAttributes.Usage.Position + VertexAttributes.Usage.ColorPacked + VertexAttributes.Usage.Normal + VertexAttributes.Usage.TextureCoordinates;
+
+        return modelBuilder.createCylinder(radius, height, radius, Math.max(1, (int)(6 * (float)Math.cbrt(10 * radius))), material, usageCode);
+    }
+}
